62641b4202f7937de83724877fe5b5008f47515b
ooswaab <osabtel@outlook.jp>
2020-05-31 09:55:38 +0900

    数値と演算子の登録

--- CalGUI.py
+++ CalGUI.py
@@ -1,6 +1,41 @@
 import wx
 
 
+def click_btn(event):
+    # CalText[1].SetValue(CalText[0].GetValue())
+    NumRegster(CalText[0].GetValue())
+    print("数値演算子記録", NumEnzanlist)
+
+
+def Regster(str):
+    i = 0
+    while str[i] != "=":
+        Regsterlist.append(str[i])
+        i = i + 1
+    Regsterlist.append("=")
+
+
+def NumRegster(str):
+    Regster(str)
+    i = 0
+    while i < len(Regsterlist):
+        if '0' <= Regsterlist[i] <= '9':
+            num = ""
+            j = i
+            while j < len(Regsterlist):
+                if '0' <= Regsterlist[j] <= '9':
+                    num = num + Regsterlist[j]
+                elif Regsterlist[j] == " ":
+                    i = j - 1
+                    break
+                j = j + 1
+            NumEnzanlist.append(num)
+        else:
+            NumEnzanlist.append(Regsterlist[i])
+
+        i = i + 2
+
+
 class CalcFrame(wx.Frame):
     """
     フレームを継承したトップレベルウィンドウクラス
@@ -34,11 +69,13 @@ class Panelin(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent, wx.ID_ANY)
         calc_text = wx.TextCtrl(self, wx.ID_ANY, size=(200, 20))
+        CalText.append(calc_text)
         calc_btn = wx.Button(self, wx.ID_ANY, "計算")
         layout = wx.BoxSizer(wx.HORIZONTAL)
         layout.Add(calc_text, 1)
         layout.Add(calc_btn)
         self.SetSizer(layout)
+        calc_btn.Bind(wx.EVT_BUTTON, click_btn)
 
 
 class PanelResult(wx.Panel):
@@ -46,6 +83,7 @@ class PanelResult(wx.Panel):
         super().__init__(parent, wx.ID_ANY)
         resultlabel = wx.StaticText(self, wx.ID_ANY, "結果:")
         result_text = wx.TextCtrl(self, wx.ID_ANY, size=(280, 20))
+        CalText.append(result_text)
         layout = wx.BoxSizer(wx.VERTICAL)
         layout.Add(resultlabel)
         layout.Add(result_text)
@@ -55,6 +93,9 @@ class PanelResult(wx.Panel):
 if __name__ == '__main__':
 
     # カスタムフレームを初期化してアプリケーションを開始
+    CalText = []
+    Regsterlist = []
+    NumEnzanlist = []
     application = wx.App()
     frame = CalcFrame()
     frame.Show()
